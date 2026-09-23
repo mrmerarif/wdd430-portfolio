ff282b66fb8a4784bd21e4b7e739093b0b1eb089
mrmerarif <bflores2@byupathway.edu>
2026-09-22 22:17:59 -0600

    Add dynamic rendering to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,7 @@
 import { getProjects } from "@/lib/projects-db";
 
+export const dynamic = "force-dynamic";
+
 export default async function ProjectsOverview() {
   const projects = await getProjects();
 
